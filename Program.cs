fc9c8bc9924eda604a6e12b3b92104620d08c9fa
AethericSoftworks <143554426+AethericSoftworks@users.noreply.github.com>
2024-03-28 18:59:10 -0400

    Adding program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var process in Process.GetProcessesByName("SkyrimSE"))
+        {
+            process.Kill();
+        }
+    }
+}
